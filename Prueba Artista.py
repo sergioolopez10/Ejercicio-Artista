9b87c0901655af28c0d5f4c72e3923a3a4285163
sergioolopez10 <slopcasc@g.educaand.es>
2023-03-15 00:33:26 +0100

    Pequeños fallos arreglado
    Cuando modificaba o eliminaba a algún artista me daba error, y lo he solucionado. Además, he agregado algunas excepciones.

--- Prueba Artista.py
+++ Prueba Artista.py
@@ -43,7 +43,7 @@ class Musico(Artista):
 
 
     
-def nuevo_artista():
+def nuevo_artista(lista_artistas):
     try:
         nombre = input("¿Qué nombre tiene el artista? ")
         edad = input("¿Qué edad tiene el artista? ")
@@ -53,10 +53,14 @@ def nuevo_artista():
         if pregunta.lower() == "si":
             numerocanciones = input("¿Cuántas canciones tiene el músico? ")
             miMusico = Musico(numerocanciones, nombre, edad, nombre_profesional, estilo)
+            lista_artistas.append(miMusico)
             return miMusico
-        else:
+        if pregunta.lower() == "no":
             miArtista = Artista(nombre, edad, nombre_profesional, estilo)
+            lista_artistas.append(miArtista)
             return miArtista
+        else:
+            print ("Por favor, introduzca sólo sí o no")
     except ValueError:
         print("La edad y el número de canciones deben ser números enteros.")
 
@@ -64,7 +68,12 @@ def nuevo_artista():
 def finalizar_programa():
     print("El programa se ha finalizado")
 
-def modificar_artista(lista_artistas, nombre_artista):
+def modificar_artista(lista_artistas):
+    if not lista_artistas:
+        print("No hay artistas en la lista")
+        return
+    
+    nombre_artista = input("Ingrese el nombre del artista que desea modificar: ")
     for artista in lista_artistas:
         if artista.get_nombre() == nombre_artista:
             print("Datos actuales del artista:")
@@ -76,14 +85,17 @@ def modificar_artista(lista_artistas, nombre_artista):
                 print(f"Número de canciones: {artista.get_numerocanciones()}")
 
             numerocanciones = None
-            if isinstance(artista, Musico):
-                numerocanciones = input("Ingrese el nuevo número de canciones: ")
-                artista.set_numerocanciones(numerocanciones)
-
+            
             nombre = input("Ingrese el nuevo nombre del artista: ")
             edad = input("Ingrese la nueva edad del artista: ")
             nombre_profesional = input("Ingrese el nuevo nombre profesional del artista: ")
             estilo = input("Ingrese el nuevo estilo del artista: ")
+            if isinstance(artista, Musico):
+                try:
+                    numerocanciones = input("Ingrese el nuevo número de canciones: ")
+                    artista.set_numerocanciones(numerocanciones)
+                except ValueError:
+                    print("El número de canciones debe ser un número entero.")
             artista.set_nombre(nombre)
             artista.set_edad(edad)
             artista.set_nombre_profesional(nombre_profesional)
@@ -96,27 +108,30 @@ def modificar_artista(lista_artistas, nombre_artista):
                     print(f"Nombre del músico: {artista.get_nombre()}, Edad del músico: {artista.get_edad()}, Nombre profesional del músico: {artista.get_nombre_profesional()}, Estilo del músico: {artista.get_estilo()}, Número de canciones que tiene: {artista.get_numerocanciones()}")
                 else:
                     print(f"Nombre del artista: {artista.get_nombre()}, Edad del artista: {artista.get_edad()}, Nombre profesional del artista: {artista.get_nombre_profesional()}, Estilo del artista: {artista.get_estilo()}")
-
-            return
-
-    print("No se encontró un artista con ese nombre.")
+        else:
+            print("No se encontró un artista con ese nombre.")
 
 
-def eliminar_artista(lista_artistas, nombre_artista):
+def eliminar_artista(lista_artistas):
+    if not lista_artistas:
+        print("No hay artistas en la lista")
+        return
+    nombre_artista = input("Ingrese el nombre del artista que desea eliminar: ")
+    encontrado=False
     for i, artista in enumerate(lista_artistas):
         if artista.get_nombre() == nombre_artista:
             del lista_artistas[i]
             print("Artista eliminado con éxito.")
             print("Lista de artistas actualizada:")
+            encontrado=True
             for artista in lista_artistas:
                 if isinstance(artista, Musico):
                     print(f"Nombre del músico: {artista.get_nombre()}, Edad del músico: {artista.get_edad()}, Nombre profesional del músico: {artista.get_nombre_profesional()}, Estilo del músico: {artista.get_estilo()}, Número de canciones que tiene: {artista.get_numerocanciones()}")
                 else:
                     print(f"Nombre del artista: {artista.get_nombre()}, Edad del artista: {artista.get_edad()}, Nombre profesional del artista: {artista.get_nombre_profesional()}, Estilo del artista: {artista.get_estilo()}")
+    if not encontrado: 
+        print("No hay ningún artista con ese nombre")
 
-            return
-
-    print("No se encontró un artista con ese nombre.")
 
 
 def main():
@@ -126,16 +141,18 @@ def main():
         try:
             P=int(input("Si desea añadir un artista:1. Si desea modificar un artista:2. Si desea eliminar un artista:3 Si desea finalizar el programa:4 "))
             if P==1:
-                nuevo_artista()
+                nuevo_artista(lista_artistas)
             if P==2:
-                modificar_artista(lista_artistas, nombre_artista)
+                modificar_artista(lista_artistas)
             if P==3:
-                eliminar_artista(lista_artistas, nombre_artista)
+                eliminar_artista(lista_artistas)
             if P==4:
                 finalizar_programa()
                 break
+            else:
+                print("Por favor, introduza un número entre 1 y 4 en función de lo que desee hacer.")
         except ValueError:
-            print("Por favor, introduzca un número entre 1 y 4 en función de lo que desee hacer")
+            print("Por favor, introduzca un número entre 1 y 4 en función de lo que desee hacer.")
 
 
 if __name__=='__main__':
