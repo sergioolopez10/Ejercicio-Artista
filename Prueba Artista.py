e61a40e3a49564ee349dbd05e3c80440718d88c6
sergioolopez10 <slopcasc@g.educaand.es>
2023-03-14 20:28:41 +0100

    Función para modificar artistas
    Función que permite cambiar los atributos del artista o músico que hayamos introducido previamente

--- Prueba Artista.py
+++ Prueba Artista.py
@@ -62,4 +62,41 @@ def nuevo_artista():
 def finalizar_programa():
     print("El programa se ha finalizado")
 
+def modificar_artista(lista_artistas, nombre_artista):
+    for artista in lista_artistas:
+        if artista.get_nombre() == nombre_artista:
+            print("Datos actuales del artista:")
+            print(f"Nombre: {artista.get_nombre()}")
+            print(f"Edad: {artista.get_edad()}")
+            print(f"Nombre profesional: {artista.get_nombre_profesional()}")
+            print(f"Estilo: {artista.get_estilo()}")
+            if isinstance(artista, Musico):
+                print(f"Número de canciones: {artista.get_numerocanciones()}")
+
+            numerocanciones = None
+            if isinstance(artista, Musico):
+                numerocanciones = input("Ingrese el nuevo número de canciones: ")
+                artista.set_numerocanciones(numerocanciones)
+
+            nombre = input("Ingrese el nuevo nombre del artista: ")
+            edad = input("Ingrese la nueva edad del artista: ")
+            nombre_profesional = input("Ingrese el nuevo nombre profesional del artista: ")
+            estilo = input("Ingrese el nuevo estilo del artista: ")
+            artista.set_nombre(nombre)
+            artista.set_edad(edad)
+            artista.set_nombre_profesional(nombre_profesional)
+            artista.set_estilo(estilo)
+
+            print("Artista modificado con éxito.")
+            print("Lista de artistas actualizada:")
+            for artista in lista_artistas:
+                if isinstance(artista, Musico):
+                    print(f"Nombre del músico: {artista.get_nombre()}, Edad del músico: {artista.get_edad()}, Nombre profesional del músico: {artista.get_nombre_profesional()}, Estilo del músico: {artista.get_estilo()}, Número de canciones que tiene: {artista.get_numerocanciones()}")
+                else:
+                    print(f"Nombre del artista: {artista.get_nombre()}, Edad del artista: {artista.get_edad()}, Nombre profesional del artista: {artista.get_nombre_profesional()}, Estilo del artista: {artista.get_estilo()}")
+
+            return
+
+    print("No se encontró un artista con ese nombre.")
+
 
